0626ebc77bc6644cbf8ef44dead36fa64f38d741
Roselie Ann <216465805+RoselieDigitals@users.noreply.github.com>
2025-07-01 22:03:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const firebaseConfig = {
   storageBucket: "roselie-digitals-4ece4.appspot.com",
   messagingSenderId: "506496490813",
   appId: "1:506496490813:web:443b255a18bfcd08ce3954",
-  databaseURL: "https://taxiuptax-default-rtdb.firebaseio.com",
+  databaseURL: "https://roselie-digitals-4ece4-default-rtdb.firebaseio.com",
 };
 
 // ⚙️ Initialize Firebase
